6d30f2979901094221b078ad39462afef288f7f3
Josu <josuesbla@gmail.com>
2026-02-16 13:07:53 +0100

    Fix GitHub Pages configuration

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,12 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
     output: "export",
-    basePath: isProd ? "/md-blog-mapper" : "",
-    assetPrefix: isProd ? "/md-blog-mapper/" : "",
+    basePath: "/md-blog-mapper",
+    assetPrefix: "/md-blog-mapper",
     images: {
         unoptimized: true,
     },
-    distDir: "out",
 };
 
 export default nextConfig;
